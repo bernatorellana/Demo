aecea74a3dc377010ca2936e0e86de15ca63d4be
Bernat <bernat.orellana@gmail.com>
2020-10-02 07:52:14 +0200

    the other

--- src/projecte/Projecte.java
+++ src/projecte/Projecte.java
@@ -15,7 +15,7 @@ public class Projecte {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("THE OTHER");
     }
     
 }
